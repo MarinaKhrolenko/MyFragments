e10fe829520a356e9d96cb6707ca6080a31f9816
MarinaKhrolenko <rubka-m@mail.ru>
2015-02-03 08:48:00 +0200

    initial commit

---
+++ app/src/main/java/com/marinakhrolenko/myfragments/MainActivity.java
@@ -0,0 +1,18 @@
+package com.marinakhrolenko.myfragments;
+
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/com/marinakhrolenko/myfragments/fragments/Fragment2.java
@@ -0,0 +1,19 @@
+package com.marinakhrolenko.myfragments.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.marinakhrolenko.myfragments.R;
+
+/**
+ * Created by Администратор on 02.02.2015.
+ */
+public class Fragment2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment2,container, false);
+    }
+}
